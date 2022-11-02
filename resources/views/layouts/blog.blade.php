1d821c127d68b280654455fdb41f43adc3f2d728
Umair <um4ir.ahmad@gmail.com>
2022-11-02 13:20:54 +0500

    modified:   public/js/app.js 	modified:   resources/views/layouts/app.blade.php 	modified:   resources/views/layouts/blog.blade.php

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -46,6 +46,8 @@
         </div>
 
 
-        
+        <!-- TailwindCss js bundle -->
+        <script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"></script>
+
     </body>
 </html>
